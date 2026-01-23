9b45ae3cf6d8a68c222ec8b784de106cd4f638e0
khushc2007 <khushhc1983@gmail.com>
2026-01-23 22:16:25 +0530

    Update Aquaculture.tsx

--- src/pages/applications/Aquaculture.tsx
+++ src/pages/applications/Aquaculture.tsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function Aquaculture() {
+  return (
+    <div style={{ padding: 32, color: "#e6f1ff" }}>
+      <h1>Aquaculture</h1>
+      <p>Water quality monitoring for fish & shrimp farming.</p>
+    </div>
+  );
+}
