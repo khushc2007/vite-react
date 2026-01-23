728706a9ac5810139ffd09a6539ce81f7f3f8842
Khushchadha-07 <khushhc2007@gmail.com>
2026-01-23 19:00:49 +0530

    Update Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,1 +1,12 @@
-
+export default function Settings() {
+  return (
+    <>
+      <h2>Settings</h2>
+      <ul>
+        <li>Research (placeholder)</li>
+        <li>FAQ (placeholder)</li>
+        <li>Update Data (placeholder)</li>
+      </ul>
+    </>
+  );
+}
