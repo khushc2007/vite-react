a8258f98f8c1b9acce789a572731089532c1491f
Khushchadha-07 <khushhc2007@gmail.com>
2026-01-23 19:01:07 +0530

    Update MetricCard.tsx

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -1,1 +1,23 @@
+interface Props {
+  title: string;
+  value: number;
+  onClick: () => void;
+}
 
+export default function MetricCard({ title, value, onClick }: Props) {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        background: "#020617",
+        padding: 20,
+        borderRadius: 8,
+        cursor: "pointer",
+        minWidth: 120
+      }}
+    >
+      <h4>{title}</h4>
+      <p style={{ fontSize: 22 }}>{value}</p>
+    </div>
+  );
+}
