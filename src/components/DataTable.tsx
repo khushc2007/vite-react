3c5db6cc0ba599c17c6d5dd9c27c2f6d9216b3dd
Khushchadha-07 <khushhc2007@gmail.com>
2026-01-23 19:01:22 +0530

    Update DataTable.tsx

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,1 +1,26 @@
+interface Props {
+  ph: number;
+  tds: number;
+  turbidity: number;
+}
 
+export default function DataTable({ ph, tds, turbidity }: Props) {
+  return (
+    <table style={{ marginTop: 20, width: "100%", color: "#fff" }}>
+      <thead>
+        <tr>
+          <th>pH</th>
+          <th>TDS</th>
+          <th>Turbidity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{ph}</td>
+          <td>{tds}</td>
+          <td>{turbidity}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
