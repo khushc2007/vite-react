8aa2bb5bcb7bab23b385baa92732227dc3d4ef07
khushc2007 <khushhc1983@gmail.com>
2026-01-23 22:51:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./MainLayout";
 
 import Home from "./pages/Home";
 import LiveDashboard from "./pages/LiveDashboard";
