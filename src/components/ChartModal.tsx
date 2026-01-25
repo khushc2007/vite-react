7dcf917116889d0e72977a49dabb9c8b826ba009
khushc2007 <khushhc1983@gmail.com>
2026-01-25 16:41:37 +0530

    Update ChartModal.tsx

--- src/components/ChartModal.tsx
+++ src/components/ChartModal.tsx
@@ -8,7 +8,8 @@ import {
   ResponsiveContainer,
   ReferenceLine,
 } from "recharts";
-import { toPng } from "html-to-image";
+import * as htmlToImage from 
+  "html-to-image";
 import { useRef } from "react";
 
 type Props = {
