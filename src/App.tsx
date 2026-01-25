386ba3feabd7d1511db44bf5316d7df2ab9cfe7d
khushc2007 <khushhc1983@gmail.com>
2026-01-25 22:54:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,66 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./MainLayout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "./pages/Home";
+/* SIDEBAR */
+import Sidebar from "./components/Sidebar";
+
+/* PAGES */
 import LiveDashboard from "./pages/LiveDashboard";
 import History from "./pages/History";
 import Settings from "./pages/Settings";
 
-import Applications from "./pages/applications/Applications";
+/* APPLICATION PAGES */
 import Aquaculture from "./pages/applications/Aquaculture";
 import Agriculture from "./pages/applications/Agriculture";
 import Industrial from "./pages/applications/Industrial";
 
-import ErrorBoundary from "./components/ErrorBoundary";
-
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          {/* HOME PAGE */}
-          <Route path="/" element={<Home />} />
+      <div
+        style={{
+          display: "flex",
+          minHeight: "100vh",
+          background: "#020617",
+        }}
+      >
+        {/* SIDEBAR */}
+        <Sidebar />
 
-          {/* LIVE DASHBOARD PAGE */}
-          <Route
-            path="/live"
-            element={
-              <ErrorBoundary>
-                <LiveDashboard />
-              </ErrorBoundary>
-            }
-          />
+        {/* MAIN CONTENT */}
+        <div
+          style={{
+            flex: 1,
+            overflowY: "auto",
+          }}
+        >
+          <Routes>
+            {/* DEFAULT */}
+            <Route path="/" element={<Navigate to="/live" replace />} />
 
-          {/* HISTORY PAGE */}
-          <Route path="/history" element={<History />} />
+            {/* CORE */}
+            <Route path="/live" element={<LiveDashboard />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/settings" element={<Settings />} />
 
-          {/* SETTINGS PAGE */}
-          <Route path="/settings" element={<Settings />} />
+            {/* APPLICATIONS */}
+            <Route
+              path="/applications/aquaculture"
+              element={<Aquaculture />}
+            />
+            <Route
+              path="/applications/agriculture"
+              element={<Agriculture />}
+            />
+            <Route
+              path="/applications/industrial"
+              element={<Industrial />}
+            />
 
-          {/* APPLICATIONS SECTION */}
-          <Route path="/applications" element={<Applications />} />
-          <Route path="/applications/aquaculture" element={<Aquaculture />} />
-          <Route path="/applications/agriculture" element={<Agriculture />} />
-          <Route path="/applications/industrial" element={<Industrial />} />
-        </Route>
-      </Routes>
+            {/* FALLBACK */}
+            <Route path="*" element={<Navigate to="/live" replace />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
